851d064d29927760d47f25062ca3d7ddcf258ec1
Panthuncia <matthew.h.gomes@gmail.com>
2024-04-13 03:29:53 -0400

    fix turn icons

--- lib/widgets/icons.dart
+++ lib/widgets/icons.dart
@@ -18,11 +18,9 @@ import 'package:flutter/widgets.dart';
 class MyFlutterApp {
   MyFlutterApp._();
 
-  static const _kFontFam = 'buoy';
+  static const _kFontFam = 'MyFlutterApp';
   static const String? _kFontPkg = null;
 
   static const IconData u_turn =
-      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData buoy =
       IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
