1348fd1021f97cd26a5bcf1a83ea2ab527f58f1e
Matthew Gomes <matthew.h.gomes@gmail.com>
2024-06-01 16:57:59 -0400

    change default hints

--- lib/widgets/settings_drawer.dart
+++ lib/widgets/settings_drawer.dart
@@ -6,9 +6,9 @@ import 'package:sailbot_telemetry_flutter/utils/github_helper.dart';
 import 'package:sailbot_telemetry_flutter/widgets/cv_settings.dart';
 import 'dart:developer' as dev;
 
-final vfForwardMagnitudeProvider = StateProvider<String>((ref) => '1.0');
-final rudderKPProvider = StateProvider<String>((ref) => '1.0');
-final rudderKDProvider = StateProvider<String>((ref) => '10.0');
+final vfForwardMagnitudeProvider = StateProvider<String>((ref) => '2.0');
+final rudderASProvider = StateProvider<String>((ref) => '0.05');
+final rudderOBProvider = StateProvider<String>((ref) => '50000.0');
 
 class SettingsDrawer extends ConsumerWidget {
   const SettingsDrawer({super.key});
@@ -19,8 +19,8 @@ class SettingsDrawer extends ConsumerWidget {
     final networkComms = ref.watch(networkCommsProvider);
 
     final lastVFForwardMagnitude = ref.watch(vfForwardMagnitudeProvider);
-    final lastRudderKP = ref.watch(rudderKPProvider);
-    final lastRudderKD = ref.watch(rudderKDProvider);
+    final lastRudderKP = ref.watch(rudderASProvider);
+    final lastRudderKD = ref.watch(rudderOBProvider);
     return Drawer(
       child: ListView(
         children: [
@@ -49,7 +49,7 @@ class SettingsDrawer extends ConsumerWidget {
                 FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*'))
               ],
               onSubmitted: ((String value) {
-                ref.read(rudderKPProvider.notifier).state = value;
+                ref.read(rudderASProvider.notifier).state = value;
                 networkComms?.setRudderAdjustmentScale(double.parse(value));
               }),
             ),
@@ -64,7 +64,7 @@ class SettingsDrawer extends ConsumerWidget {
                 FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*'))
               ],
               onSubmitted: ((String value) {
-                ref.read(rudderKDProvider.notifier).state = value;
+                ref.read(rudderOBProvider.notifier).state = value;
                 networkComms?.setRudderOvershootBias(double.parse(value));
               }),
             ),
